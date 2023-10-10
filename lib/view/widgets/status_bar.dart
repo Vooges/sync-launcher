0c5bbd6f50993176661888a6f4ee182d985ff676
Jesse Vooges <jessevooges@gmail.com>
2023-10-10 23:46:42 +0200

    Improved styling of the status bar by changing font sizes

--- lib/view/widgets/status_bar.dart
+++ lib/view/widgets/status_bar.dart
@@ -9,7 +9,9 @@ class StatusBar extends StatelessWidget {
     return Row(
         children: <Widget>[
         IconButton(
-          onPressed: (){}, 
+          onPressed: (){
+            // TODO: Check what this should do: go to home screen, go to settings or open a side menu?
+          }, 
           icon: Image.asset(
             'assets/images/logo/small_rounded.png', 
             height: 60, 
@@ -17,67 +19,44 @@ class StatusBar extends StatelessWidget {
         ),
         const Spacer(),
         IconButton(
-          onPressed: (){}, 
+          onPressed: (){
+            // TODO: Check if this actually needs to do anything, or if it is just an informative thing.
+          }, 
           icon: const Icon(Icons.network_wifi_3_bar), 
-          color: Colors.white
+          color: Colors.white,
+          iconSize: 25,
         ),
         IconButton(
-          onPressed: (){}, 
+          onPressed: (){
+            // TODO: Go to the chat page.
+          }, 
           icon: const Icon(Icons.chat), 
-          color: Colors.white
+          color: Colors.white,
+          iconSize: 25,
         ),
         StreamBuilder(
           stream: Stream.periodic(const Duration(seconds: 1)),
           builder: (context, snapshot) {
-            return Text(DateFormat('HH:mm').format(DateTime.now()));
+            return Text(
+              DateFormat('HH:mm').format(DateTime.now()),
+              style: const TextStyle(fontSize: 18),
+            );
           },
         ),
         IconButton(
-          onPressed: (){}, 
-          icon: Image.asset(
-            'assets/images/profile/garf.jpg', 
-            height: 60, 
-            width: 60
+          onPressed: (){
+            // TODO: Go to account page.
+          }, 
+          icon: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset(
+              'assets/images/profile/garf.jpg', 
+              height: 60, 
+              width: 60
+            )
           )
         )
       ],
-    );
-
-    return AppBar(
-      leading: IconButton(
-        onPressed: (){}, 
-        icon: Image.asset('assets/images/logo/small_rounded.png')
-      ),
-      leadingWidth: 80,
-      toolbarHeight: 70,
-      //title: Image.asset('assets/images/logo/small_rounded.png', height: 60, width: 60),
-      actions: <Widget>[
-        IconButton(
-          onPressed: (){}, 
-          icon: const Icon(Icons.network_wifi_3_bar), 
-          color: Colors.white
-        ),
-        IconButton(
-          onPressed: (){}, 
-          icon: const Icon(Icons.chat), 
-          color: Colors.white
-        ),
-        StreamBuilder(
-          stream: Stream.periodic(const Duration(seconds: 1)),
-          builder: (context, snapshot) {
-            return Text(DateFormat('HH:mm').format(DateTime.now()));
-          },
-        ),
-        IconButton(
-          onPressed: (){}, 
-          icon: Image.asset('assets/images/profile/garf.jpg', 
-            height: 60, 
-            width: 60
-          )
-        )
-      ],
-      backgroundColor: Colors.black,
     );
   }
-
 }
