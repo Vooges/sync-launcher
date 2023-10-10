b3cd15bafa81266f3def4b167ad339d9bd51ea17
Jesse Vooges <jessevooges@gmail.com>
2023-10-10 14:44:20 +0200

    Forgot to commit this file

--- lib/retrievers/local/epic_games/local_epic_games_retriever.dart
+++ lib/retrievers/local/epic_games/local_epic_games_retriever.dart
@@ -7,7 +7,7 @@ import 'package:collection/collection.dart'; // https://stackoverflow.com/questi
 import 'package:sync_launcher/models/dlc_info.dart';
 import 'package:sync_launcher/models/game_info.dart';
 import 'package:sync_launcher/models/launcher_info.dart';
-import 'package:sync_launcher/retrievers/base_local_game_retriever.dart';
+import 'package:sync_launcher/retrievers/local/base_local_game_retriever.dart';
 
 class LocalEpicGamesRetriever extends BaseLocalGameRetriever {
   LocalEpicGamesRetriever() : super(manifestLocation: 'C:\\ProgramData\\Epic\\EpicGamesLauncher\\Data\\Manifests');
